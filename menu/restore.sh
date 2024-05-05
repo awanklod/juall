a14c77a60b94e1df625fcdca73e4e305b46da511
awanklod <137737752+awanklod@users.noreply.github.com>
2024-05-05 11:38:23 +0700

    Update restore.sh

--- menu/restore.sh
+++ menu/restore.sh
@@ -10,7 +10,7 @@ WH='\033[1;37m'
 ipsaya=$(wget -qO- ipinfo.io/ip)
 data_server=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
 date_list=$(date +"%Y-%m-%d" -d "$data_server")
-data_ip="https://raw.githubusercontent.com/awanklod/izin_reaz/main/izin"
+data_ip="https://raw.githubusercontent.com/awanklod/izin_jual/main/ip"
 checking_sc() {
 useexp=$(curl -sS $data_ip | grep $ipsaya | awk '{print $3}')
 if [[ $date_list < $useexp ]]; then
@@ -85,7 +85,7 @@ WH='\033[1;37m'
 ipsaya=$(wget -qO- ipv4.icanhazip.com)
 data_server=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
 date_list=$(date +"%Y-%m-%d" -d "$data_server")
-data_ip="https://raw.githubusercontent.com/awanklod/izin_reaz/main/izin"
+data_ip="https://raw.githubusercontent.com/awanklod/izin_jual/main/ip"
 checking_sc() {
 useexp=$(curl -sS $data_ip | grep $ipsaya | awk '{print $3}')
 if [[ $date_list < $useexp ]]; then
@@ -161,7 +161,7 @@ WH='\033[1;37m'
 ipsaya=$(wget -qO- ipv4.icanhazip.com)
 data_server=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
 date_list=$(date +"%Y-%m-%d" -d "$data_server")
-data_ip="https://raw.githubusercontent.com/awanklod/izin_reaz/main/izin"
+data_ip="https://raw.githubusercontent.com/awanklod/izin_jual/main/ip"
 checking_sc() {
 useexp=$(curl -sS $data_ip | grep $ipsaya | awk '{print $3}')
 if [[ $date_list < $useexp ]]; then
